2262aca1b3eb3d7ba389d26f26f5ee36406ed017
ROYNITISH <nitishroy067@gmail.com>
2022-01-26 22:42:28 +0530

    minor change

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error , and some changes');
+  res.render('error , and some changes , okay another change');
 });
 
 module.exports = app;
